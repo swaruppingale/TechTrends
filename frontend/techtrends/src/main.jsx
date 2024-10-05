9404b44d6f45f938ae585b4939526d3cac689ed3
Swarup Pingale <122713963+swaruppingale@users.noreply.github.com>
2024-10-06 00:00:17 +0530

    feat:sample

--- frontend/techtrends/src/main.jsx
+++ frontend/techtrends/src/main.jsx
@@ -4,8 +4,10 @@ import App from './App.jsx'
 import './index.css'
 import SignIn from './pages/SignIn.jsx'
 import Home from './pages/Home.jsx'
+import SignUp from './pages/SignUp.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <SignIn />
+    
+    <SignUp/>
   </StrictMode>,
 )
--- frontend/techtrends/src/pages/SignIn.jsx
+++ frontend/techtrends/src/pages/SignIn.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import BackgroundImg from "../assets/images/background.svg";
+import BackgroundImg from '../assets/images/background.svg';
+
 const SignIn = () => {
   return (
-    <div className="bg-green-900 flex items-center justify-center min-h-screen">
+    <div
+      className="flex items-center justify-center min-h-screen bg-cover bg-center"
+      style={{ backgroundImage: `url(${BackgroundImg})` }}
+    >
       <div className="bg-green-900 text-white text-center p-8 rounded-lg shadow-lg w-full max-w-sm">
         <h1 className="text-3xl font-bold mb-2">TechTrends</h1>
         <p className="mb-6">It's modular and designed to last</p>
-        <img
-          src={BackgroundImg}
-          alt="DJ wearing headphones"
-          className="mb-6 rounded-lg"
-        />
         <form className="space-y-4">
           <div className="relative">
             <input
@@ -44,6 +43,6 @@ const SignIn = () => {
       </div>
     </div>
   );
-}
+};
 
 export default SignIn;
