3d29204e3ecaab0be04ed763b7d1e24bea9efe32
Smitpatil0411 <smit.p@somaiya.edu>
2024-10-08 00:04:43 +0530

    feat:h

--- frontend/techtrends/src/main.jsx
+++ frontend/techtrends/src/main.jsx
@@ -4,8 +4,9 @@ import App from './App.jsx'
 import './index.css'
 import SignIn from './pages/SignIn.jsx'
 import Home from './pages/Home.jsx'
+import Homef from './pages/Homef.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <SignIn />
+    <Home />
   </StrictMode>,
 )
---
+++ frontend/techtrends/src/pages/Homef.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import BGI from '../assets/images/background.svg';
+const Homef = () => {
+  return (
+    // <div className="h-[100vh] bg-blue-100  flex items-start justify-center overflow-hidden w-[100vw]">
+    //   <div className="w-[100%] space-y-8">
+    //     hello
+    //   </div>
+    // </div>
+    <div className="bg-blue-10fixed top-0 left-0 w-full flex justify-around bg-blue-100 z-50">
+        <div>hello</div>
+    </div>
+  );
+};
+
+export default Homef;
